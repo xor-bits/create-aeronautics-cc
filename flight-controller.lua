ee2dfab0a09d42ad498018113e4eee95aac8b40f
xor-bits <oneleh@utu.fi>
2026-05-21 02:27:42 +0300

    feat: tilt input

--- flight-controller.lua
+++ flight-controller.lua
@@ -17,7 +17,7 @@ local gimbal                          = find_relay("redstone_relay_3")
 local compass                         = find_relay("redstone_relay_7")
 local tilt_controller                 = find_relay("redstone_relay_4")
 local rotation_velocity_controller    = find_relay("redstone_relay_6")
--- local input                           = find_relay("redstone_relay_5")
+local input                           = find_relay("redstone_relay_8")
 
 local gimbal_x_offset                 = 0
 local gimbal_z_offset                 = 0
@@ -25,35 +25,60 @@ local gimbal_proportional_coefficient = 1
 local gimbal_derivative_coefficient   = 6
 local gimbal_correction_power_divisor = 4
 
-local target_bearing = 0
-local holding_left = false
-local holding_right = false
+local target_bearing                  = 0
+local holding_left                    = false
+local holding_right                   = false
+local target_x_vel                    = 0
+local target_z_vel                    = 0
 
-local tau = math.pi * 2
+local tau                             = math.pi * 2
+local monitor_size                    = monitor.getSize()
+
+local strafe_left                     = 1
+local strafe_right                    = 2
+local forward                         = 3
+local backward                        = 4
+local rotate_left                     = 5
+local rotate_right                    = 6
+local inputs                          = { false, false, false, false, false, false }
+local prev_inputs                     = { false, false, false, false, false, false }
 
 local prev_x_angle, prev_z_angle      = find_angle(gimbal)
 while true do
-  local x_angle, z_angle = find_angle(gimbal)
+  local x_angle, z_angle     = find_angle(gimbal)
   local y_angle_0, y_angle_1 = find_angle(compass)
-  local target_y_angle_0 = math.cos(target_bearing)
-  local target_y_angle_1 = math.sin(target_bearing)
+  local target_y_angle_0     = math.cos(target_bearing)
+  local target_y_angle_1     = math.sin(target_bearing)
   -- local bearing = math.atan2(y_angle_1, y_angle_0)
 
-  if redstone.getInput("left") then
-    if not holding_left then
-      target_bearing = target_bearing + 0.1
-      holding_left = true
-    end
-  else
-    holding_left = false
+  inputs[strafe_left]        = input.getInput("left")
+  inputs[strafe_right]       = input.getInput("right")
+  inputs[forward]            = input.getInput("front")
+  inputs[backward]           = input.getInput("back")
+  inputs[rotate_left]        = redstone.getInput("left")
+  inputs[rotate_right]       = redstone.getInput("right")
+
+  if inputs[rotate_left] and not prev_inputs[rotate_left] then
+    target_bearing = target_bearing + 0.1
   end
-  if redstone.getInput("right") then
-    if not holding_right then
-      target_bearing = target_bearing - 0.1
-      holding_right = true
-    end
-  else
-    holding_right = false
+  if inputs[rotate_right] and not prev_inputs[rotate_right] then
+    target_bearing = target_bearing - 0.1
+  end
+  if inputs[forward] and not prev_inputs[forward] then
+    target_z_vel = target_z_vel - 1
+  end
+  if inputs[backward] and not prev_inputs[backward] then
+    target_z_vel = target_z_vel + 1
+  end
+  if inputs[strafe_left] and not prev_inputs[strafe_left] then
+    target_x_vel = target_x_vel + 1
+  end
+  if inputs[strafe_right] and not prev_inputs[strafe_right] then
+    target_x_vel = target_x_vel - 1
+  end
+
+  for i = 1, 6 do
+    prev_inputs[i] = inputs[i]
   end
 
   -- front output is back  right propeller
@@ -64,16 +89,16 @@ while true do
   local x_delta = x_angle - prev_x_angle
   local z_delta = z_angle - prev_z_angle
   local x_corr = (gimbal_proportional_coefficient * x_angle
-        + gimbal_derivative_coefficient * x_delta
-        + gimbal_x_offset) / gimbal_correction_power_divisor
+    + gimbal_derivative_coefficient * x_delta
+    + gimbal_x_offset + target_x_vel) / gimbal_correction_power_divisor
   local z_corr = (gimbal_proportional_coefficient * z_angle +
-        gimbal_derivative_coefficient * z_delta +
-        gimbal_z_offset) / gimbal_correction_power_divisor
+    gimbal_derivative_coefficient * z_delta +
+    gimbal_z_offset + target_z_vel) / gimbal_correction_power_divisor
   local dot = y_angle_0 * target_y_angle_1 + y_angle_1 * target_y_angle_0
 
-  tilt_controller.setAnalogOutput("back", math.min(15, math.max(0,  -x_corr + z_corr)))
-  tilt_controller.setAnalogOutput("left", math.min(15, math.max(0,   x_corr + z_corr)))
-  tilt_controller.setAnalogOutput("front", math.min(15, math.max(0,  x_corr - z_corr)))
+  tilt_controller.setAnalogOutput("back", math.min(15, math.max(0, -x_corr + z_corr)))
+  tilt_controller.setAnalogOutput("left", math.min(15, math.max(0, x_corr + z_corr)))
+  tilt_controller.setAnalogOutput("front", math.min(15, math.max(0, x_corr - z_corr)))
   tilt_controller.setAnalogOutput("right", math.min(15, math.max(0, -x_corr - z_corr)))
   print("\n\n\nINFO")
   print("x_angle:", x_angle, "z_angle:", z_angle)
@@ -87,20 +112,26 @@ while true do
 
   -- back  left  -- turns left    -- decreases angle
   -- front right -- turns right   -- increases angle
-  -- left  right -- goes forwards -- 
-  -- front back  -- goes backwads -- 
+  -- left  right -- goes forwards --
+  -- front back  -- goes backwads --
 
+  local bearing_draw_x_mid = 14
+  local bearing_draw_y_mid = 10
   monitor.clear()
-  monitor.setCursorPos(1,1)
-  local bearing_draw_x_offs = 14
-  for i=1,bearing_draw_x_offs do
-    monitor.write(" ")
-  end
-  monitor.write("^")
-  monitor.setCursorPos(1,2)
-  for i=1,(bearing_draw_x_offs - dot) do
-    monitor.write(" ")
-  end
+  monitor.setCursorPos(1, 1)
+  monitor.setCursorPos(bearing_draw_x_mid, 1)
+  monitor.write("|")
+  monitor.setCursorPos(math.max(1, bearing_draw_x_mid - dot), 2)
+  monitor.write("x")
+  monitor.setCursorPos(bearing_draw_x_mid, 3)
+  monitor.write("|")
+
+  monitor.setCursorPos(bearing_draw_x_mid, bearing_draw_y_mid)
+  monitor.write(".")
+  monitor.setCursorPos(
+    math.max(1, bearing_draw_x_mid - target_x_vel),
+    math.max(4, bearing_draw_y_mid + target_z_vel)
+  )
   monitor.write("x")
 
   local turn_left = false
